0026dfe5cecdc8d5e1d4168d53d1d2301e4fc62d
Amy Chin-Lai <amy.chinlai@gmail.com>
2020-05-11 14:34:11 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -63,3 +63,17 @@ end
     turn(board)
   end
 end
+
+def turn_count(board)
+  turn=0
+  board.each do |token|
+    if(token=="X" || token=="O")
+      turn+=1
+end
+end
+turn
+end
+
+def current_player(board)
+  turn_count(board) % 2 == 0? "X": "O"
+end

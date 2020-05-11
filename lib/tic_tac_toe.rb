117ec5cfcc0ac29b4c4bcddb401323fb17440fa1
Amy Chin-Lai <amy.chinlai@gmail.com>
2020-05-11 15:06:21 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -57,7 +57,7 @@ end
     input = gets.strip
     index = input_to_index(input)
     if valid_move?(board,index) == true
-    move(board, index, "X")
+    move(board, index, value)
     display_board(board)
   else
     turn(board)
